ee92f01f0aa695fc9323dfbb2cc9686fdd67d35d
Lche <m506108@outlook.com>
2025-03-25 10:55:57 +1100

    response_model include & exclude

--- M_Python/FastAPI/request&response07/apps/app08.py
+++ M_Python/FastAPI/request&response07/apps/app08.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi import APIRouter
 from pydantic import BaseModel, EmailStr
-from typing import Union
+from typing import Union,List
+
+
 app08 = APIRouter()
 
 class UserIn(BaseModel):
@@ -17,8 +19,34 @@ class UserOut(BaseModel):
     email:EmailStr
     full_name: str | None =None
 
+class Item(BaseModel):
+    name:str
+    description:str|None = None
+    price:float
+    tax:float = 10.5
+    tags:List[str] = []
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+
 @app08.post("/user02", response_model=UserOut)
 async def get_user(user:UserIn):
     # 存到数据库
     return user
+# @app08.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+@app08.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=False, response_model_exclude_defaults=False,response_model_exclude_none=True)
+async def read_item(item_id: str):
+    return items[item_id]
 
+@app08.get("/items2{item_id}", response_model=Item, response_model_exclude={"tax"})
+async def read_item2(item_id: str):
+    return items[item_id]
+
+@app08.get("/items3/{item_id}", response_model=Item, response_model_include={"name", "price"})
+async def read_item(item_id: str):
+    return items[item_id]
